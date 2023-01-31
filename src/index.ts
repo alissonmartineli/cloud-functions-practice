da6dd5c33609bb7d5c2d155adff8534351959b5f
Alisson Martineli <martineli.alisson@gmail.com>
2023-01-31 19:54:12 -0300

    feat: setup typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export const validateTemperature = async (req, res) => {
+import { HttpFunction, Request, Response } from "@google-cloud/functions-framework/build/src/functions";
+
+export const validateTemperature: HttpFunction = async (req: Request, res: Response) => {
   try {
     if (req.body.temp < 100) {
       res.status(200).send("Temperature OK");
