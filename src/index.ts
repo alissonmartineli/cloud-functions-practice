f153e6ec8efbacafc8a26ed17ad1d0083a7abd03
Alisson Martineli <martineli.alisson@gmail.com>
2023-01-31 20:05:15 -0300

    feat: setup eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import { HttpFunction, Request, Response } from "@google-cloud/functions-framework/build/src/functions";
+import {
+  type HttpFunction,
+  type Request,
+  type Response,
+} from '@google-cloud/functions-framework/build/src/functions'
 
-export const validateTemperature: HttpFunction = async (req: Request, res: Response) => {
+export const validateTemperature: HttpFunction = async (
+  req: Request,
+  res: Response
+) => {
   try {
     if (req.body.temp < 100) {
-      res.status(200).send("Temperature OK");
+      res.status(200).send('Temperature OK')
     } else {
-      res.status(200).send("Too hot");
+      res.status(200).send('Too hot')
     }
   } catch (error) {
-    console.log("got error: ", error);
-    res.status(500).send(error);
+    console.log('got error: ', error)
+    res.status(500).send(error)
   }
-};
+}
